8ca00aca31fc0fcd5c70af0ebaf1d082c07293e2
Dennis Iversen <dennis.iversen@gmail.com>
2016-05-30 19:23:04 +0200

    fix test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 //md = require('markdown-it-bin');
-md = require('markdown-it-embed-highlight-mathjax');
+md = require('./index');
 md.options.html = true;
 
 var str = "<p>test</p># her er en test ![test](./test/test.mp4) $1 *2* 3$";
