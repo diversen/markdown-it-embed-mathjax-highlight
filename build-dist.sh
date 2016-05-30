ba12d21eea0c1597c9b5f0962abc2ef1b67fc62e
Dennis Iversen <dennis.iversen@gmail.com>
2016-05-30 19:07:58 +0200

    auto commit

--- build-dist.sh
+++ build-dist.sh
@@ -4,5 +4,5 @@ rm -rf dist
 mkdir dist
 
 ( printf "/*! ${NPM_PACKAGE} ${NPM_VERSION} ${GITHUB_PROJ} @license MIT */" ; \
-		browserify . -s markdownItBin | uglifyjs \
+		browserify . -s markdownItEmbedMathjaxHighlight | uglifyjs \
 		) > dist/markdown-it-embed-mathjax-highlight.js
